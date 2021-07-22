8b02a30d7a8befe24e1ef391636040c2c4be4f2c
Yauheniyaliashkevich <yauheniyaliashkevich@gmail.com>
2021-07-21 20:25:22 -0500

    First Commit

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -1,5 +1,11 @@
 package com.collection;
 
 public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Jane I love you!");
+        }
+
+    }
 
 }
