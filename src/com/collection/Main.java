8c9f1869fc6d499d20bf62795311eecb417a47d2
Yauheniyaliashkevich <yauheniyaliashkevich@gmail.com>
2021-07-28 11:55:01 -0500

    It should trigger the pipe line

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -2,7 +2,7 @@ package com.collection;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 8; i++) {
             System.out.println("Jane I love you!");
             Thread.sleep(1000);
         }
